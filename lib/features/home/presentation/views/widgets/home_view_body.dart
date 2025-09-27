43068ca28dd003cdac3dba7be1f343c3a7e9c724
AlhasanNO <aalhassann@gmail.com>
2025-09-27 12:35:23 +0300

    Add styles & fix animation

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,25 +1,27 @@
-import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedListView(),
-          SizedBox(height: 60.0),
-          Text('Best Seller', style: Styles.titleMedium),
-        ],
-      ),
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(),
+        FeaturedListView(),
+        SizedBox(height: 60.0),
+        Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 30.0),
+          child: Text('Best Seller', style: Styles.textStyle18),
+        ),
+        BestSellerListView(),
+      ],
     );
   }
 }
