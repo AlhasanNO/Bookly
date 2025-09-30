1372a48131cc4337862d99e29014c3f5f3522f98
AlhasanNO <aalhassann@gmail.com>
2025-09-30 19:06:38 +0300

    Create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,9 @@
-import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
-import 'package:bookly_app/features/home/presentation/views/home_view.dart';
-import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
+
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/features/search/presentation/view/search_view.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 
 abstract class AppRouter {
   static const homeView = '/home';
@@ -15,6 +17,10 @@ abstract class AppRouter {
       GoRoute(
         path: '/book_details',
         builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: '/search_view',
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
---
+++ lib/features/search/presentation/view/search_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:bookly_app/features/search/presentation/view/widgets/search_view_body.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SearchViewBody());
+  }
+}
