9923428674ce89a4d29a6001dfe5e44452528d21
AlhasanNO <aalhassann@gmail.com>
2025-10-02 21:51:00 +0300

    Setup service locator

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bookly_app/features/home/presentation/cubits/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/cubits/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,27 +16,37 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: CustomScrollView(
-        physics: BouncingScrollPhysics(),
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomHomeAppBar(),
-                FeaturedListView(),
-                SizedBox(height: 60.0),
-                Padding(
-                  padding: EdgeInsetsGeometry.symmetric(horizontal: 30.0),
-                  child: Text('Best Seller', style: Styles.textStyle18),
-                ),
-                SizedBox(height: 20.0),
-              ],
-            ),
+    return SafeArea(
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
           ),
-          SliverFillRemaining(child: BestSellerListView()),
+          BlocProvider(
+            create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+          ),
         ],
+        child: const CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomHomeAppBar(),
+                  FeaturedListView(),
+                  SizedBox(height: 60.0),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 30.0),
+                    child: Text('Best Seller', style: Styles.textStyle18),
+                  ),
+                  SizedBox(height: 20.0),
+                ],
+              ),
+            ),
+            SliverFillRemaining(child: BestSellerListView()),
+          ],
+        ),
       ),
     );
   }
